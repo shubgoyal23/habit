b70a8b2b5dfab012678a48487d0753155e3f3010
shubham g <shubhamgoyal.dev@gmail.com>
2025-01-01 18:26:38 +0530

    added delete account function

--- frontend/src/store/AuthSlice.js
+++ frontend/src/store/AuthSlice.js
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
 
 const authSlice = createSlice({
    name: "Auth",
@@ -8,16 +7,10 @@ const authSlice = createSlice({
       login(state, action) {
          state.loggedin = true;
          state.userDate = action.payload;
-         localStorage.setItem("accessToken", action.payload.accessToken);
-         localStorage.setItem("refreshToken", action.payload.refreshToken);
-         axios.defaults.headers.common["AccessToken"] = localStorage.getItem("accessToken");
-         axios.defaults.headers.common["RefreshToken"] = localStorage.getItem("refreshToken");
       },
       logout(state, action) {
          state.loggedin = false;
          state.userDate = null;
-         localStorage.removeItem("accessToken");
-         localStorage.removeItem("refreshToken");
       },
    },
 });
