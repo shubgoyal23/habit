d6b0985e7e69861376d2268fc61a59339c5ae433
shubham g <shubhamgoyal.dev@gmail.com>
2025-01-09 21:27:17 +0530

    new folder added

--- backend/src/models/streak.model.js
+++ backend/src/models/streak.model.js
@@ -1,9 +1,18 @@
 import mongoose from "mongoose";
 
+const RepeatSchema = new mongoose.Schema({
+   name: {
+      type: String,
+      enum: ["daily", "weekly", "monthly", "yearly"],
+      default: "daily",
+   },
+   value: String,
+});
+
 const streakSchema = new mongoose.Schema(
    {
       userId: {
-         type: mongoose.Schema.Types.ObjectId,
+         type: mongoose.Schema.Types.ObjectId,  // user id who has created the habit
          ref: "users",
          index: true,
       },
@@ -12,11 +21,20 @@ const streakSchema = new mongoose.Schema(
       duration: String,
       startTime: String,
       endTime: String,
+      startDate: String,
+      endDate: String,
+      repeat: RepeatSchema, // how many times to repeat
       place: String,
       how: String,
       ifthen: String,
       point: Number,
       startDate: Date,
+      steak: Number, // regular days user has completed the habit
+      habitType: {
+         type: String, // regular habit is daily , negative is negative which will be marked completed by default and user has to mark it as undone if he wants , oneTime is oneTime like todo
+         enum: ["regular", "negative", "oneTime"],
+         default: "regular",
+      },
       daysCompleted: [Date],
    },
    { timestamps: true }
