c1b6a777b2790adaeee681bb47758afae855aa59
shubham goyal <shubhamgoyal.dev@gmail.com>
2025-01-10 14:55:57 +0530

    models updated

--- worker/models/models.go
+++ worker/models/models.go
@@ -9,4 +9,26 @@ type User struct {
 	Email     string             `bson:"email"`
 	FcmToken  string             `bson:"fcmToken"`
 	Epoch     int64              `bson:"epoch"`
+	Username  string             `bson:"username"`
+	TimeZone  int                `bson:"timeZone"`
+	IsActive  bool               `bson:"isActive"`
+}
+
+type Habit struct {
+	Id          primitive.ObjectID `bson:"_id"`
+	UserId      primitive.ObjectID `bson:"userId"`
+	Name        string             `bson:"name"`
+	Description string             `bson:"description"`
+	Duration    int                `bson:"duration"`
+	StartTime   int64              `bson:"startTime"`
+	EndTime     int64              `bson:"endTime"`
+	StartDate   int64              `bson:"startDate"`
+	EndDate     int64              `bson:"endDate"`
+	Point       int                `bson:"point"`
+	Place       string             `bson:"place"`
+	Steak       string             `bson:"steak"`
+	HabitType   string             `bson:"habitType"`
+	How         string             `bson:"how"`
+	Ifthen      string             `bson:"ifthen"`
+	Notify      bool               `bson:"notify"`
 }
