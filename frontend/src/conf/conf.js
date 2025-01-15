35ce4f559a4682a7d8647933207fc36911aceb9e
shubham g <shubhamgoyal.dev@gmail.com>
2025-01-15 20:59:19 +0530

    temp

--- frontend/src/conf/conf.js
+++ frontend/src/conf/conf.js
@@ -1,3 +1,4 @@
 export const conf = {
-    BACKEND_URL: import.meta.env.VITE_BACKEND_URL,
+   BACKEND_URL: "https://api-habit.proteinslice.com",
+   // BACKEND_URL: import.meta.env.VITE_BACKEND_URL,
 };
