25c294b409e84c5b5b4e3317463e29ddcdcc0b46
shubham g <shubhamgoyal.dev@gmail.com>
2025-01-17 23:28:27 +0530

    vite build check added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+   plugins: [react()],
+   resolve: {
+      alias: {
+         "@": path.resolve(__dirname, "./src"),
+      },
+   },
+   build: {
+      rollupOptions: {
+         output: {
+            manualChunks: {
+               react: ["react", "react-dom"],
+               ui: ["react-router-dom", "react-icons"],
+            },
+         },
+      },
+      chunkSizeWarningLimit: 500,
+   },
+});
