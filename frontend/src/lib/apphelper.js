0e7997e34104015bae043189e456970832b6df7b
shubham goyal <shubhamgoyal.dev@gmail.com>
2025-01-18 09:54:01 +0530

    token fixed

--- frontend/src/lib/apphelper.js
+++ frontend/src/lib/apphelper.js
@@ -7,28 +7,28 @@ const setTokenToStorageAndAxios = async (dataRec) => {
    if (!Capacitor.isNativePlatform()) return;
    if (dataRec?.accessToken) {
       await setToken("accesstoken", dataRec?.accessToken);
-      axios.defaults.headers.common["accesstoken"] = dataRec?.accessToken;
+      axios.defaults.headers.common["Accesstoken"] = `Bearer ${dataRec?.accessToken}`;
    }
    if (dataRec?.refreshToken) {
       await setToken("refreshtoken", dataRec?.refreshToken);
-      axios.defaults.headers.common["refreshtoken"] = dataRec?.refreshToken;
+      axios.defaults.headers.common["Refreshtoken"] = `Bearer ${dataRec?.refreshToken}`;
    }
 };
 
 const SetTokenToAxios = async () => {
    if (!Capacitor.isNativePlatform()) return;
-   let accessToken = await getToken("accessToken");
-   let refreshToken = await getToken("refreshToken");
-   axios.defaults.headers.common["refreshToken"] = refreshToken;
-   axios.defaults.headers.common["accessToken"] = accessToken;
+   let accessToken = await getToken("accesstoken");
+   let refreshToken = await getToken("refreshtoken");
+   axios.defaults.headers.common["Accesstoken"] = `Bearer ${accessToken}`;
+   axios.defaults.headers.common["Refreshtoken"] = `Bearer ${refreshToken}`;
 };
 
 const clearToken = async () => {
    if (!Capacitor.isNativePlatform()) return;
    await setToken("accesstoken", "");
    await setToken("refreshtoken", "");
-   axios.defaults.headers.common["accesstoken"] = "";
-   axios.defaults.headers.common["refreshtoken"] = "";
+   axios.defaults.headers.common["Accesstoken"] = "";
+   axios.defaults.headers.common["Refreshtoken"] = "";
 };
 
 const SetTheme = async (theme) => {
