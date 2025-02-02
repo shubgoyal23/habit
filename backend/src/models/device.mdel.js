86299cfee3a396ee5c3d5a372a91ce8c11573922
shubham goyal <shubhamgoyal.dev@gmail.com>
2025-02-02 08:13:21 +0530

    added time stamp in device model

--- backend/src/models/device.mdel.js
+++ backend/src/models/device.mdel.js
@@ -1,26 +1,29 @@
 import mongoose from "mongoose";
 
-const DeviceSchema = new mongoose.Schema({
-   userid: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "users",
-      index: true,
+const DeviceSchema = new mongoose.Schema(
+   {
+      userid: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "users",
+         index: true,
+      },
+      deviceId: {
+         type: String,
+         required: true,
+         unique: true,
+         index: true,
+      },
+      model: String,
+      platform: String,
+      os: String,
+      osVersion: String,
+      manufacturer: String,
+      isVirtual: String,
+      webViewVersion: String,
+      androidSDKVersion: String,
    },
-   deviceId: {
-      type: String,
-      required: true,
-      unique: true,
-      index: true,
-   },
-   model: String,
-   platform: String,
-   os: String,
-   osVersion: String,
-   manufacturer: String,
-   isVirtual: String,
-   webViewVersion: String,
-   androidSDKVersion: String,
-});
+   { timestamps: true }
+);
 
 export const Device =
    mongoose.models.Device || mongoose.model("Device", DeviceSchema);
