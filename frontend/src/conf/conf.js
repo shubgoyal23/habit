dba1e5b8f9e409f1897bf0df23be81e36d1504b8
shubham goyal <shubhamgoyal.dev@gmail.com>
2025-04-06 22:54:38 +0530

    frontend env fix

--- frontend/src/conf/conf.js
+++ frontend/src/conf/conf.js
@@ -1,4 +1,14 @@
+const env = import.meta.env.VITE_ENV;
+
+let BACKEND_URL = "";
+
+if (env === "development") {
+   BACKEND_URL = import.meta.env.VITE_BACKEND_URL_DEVELOPMENT;
+} else {
+   BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+}
+
 export const conf = {
-   BACKEND_URL: import.meta.env.VITE_BACKEND_URL,
+   BACKEND_URL: BACKEND_URL,
    GOOGLE_CLIENT_ID: import.meta.env.VITE_GOOGLE_CLIENT_ID,
 };
